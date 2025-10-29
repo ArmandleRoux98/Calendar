1396f390d93fa88c4ab1ada1ca4cb663796810a5
ArmandleRoux98 <armand.leroux06@gmail.com>
2025-10-29 13:08:01 +0200

    Changed how patient select gets built to make function reusable

--- frontend/createBooking.js
+++ frontend/createBooking.js
@@ -1,33 +1,47 @@
-document.getElementById("diary_select").addEventListener("change", async function() {
-    const patients = await getPatients(JSON.parse(this.value).entity_uid)
-    const patientSelect = document.getElementById("patient_select");
-    patients.data.forEach(patient => {
-        const option = document.createElement("option");
-        option.value = patient.uid;
-        option.textContent = `${patient.name} ${patient.surname}`;
-        patientSelect.appendChild(option)
-    })
 
-    const bookingTypes = await getBookingTypes(JSON.parse(this.value));
-    const bookingTypeSelect = document.getElementById("booking_type");
-    console.log(bookingTypes)
-    for (const bookingType of bookingTypes.data) {
-        const option = document.createElement("option");
-        option.value = bookingType.uid;
-        option.textContent = bookingType.name;
-        bookingTypeSelect.appendChild(option);
-    }
+const diarySelect = document.getElementById("diary_select")
+if (diarySelect){
+    diarySelect.addEventListener("change", async function() {
+        await buildPatientSelect(JSON.parse(this.value).entity_uid)
+        
+        const bookingTypes = await getBookingTypes(JSON.parse(this.value));
+        const bookingTypeSelect = document.getElementById("booking_type");
+        console.log(bookingTypes)
+        for (const bookingType of bookingTypes.data.reverse()) {
+            const option = document.createElement("option");
+            option.value = bookingType.uid;
+            option.textContent = bookingType.name;
+            bookingTypeSelect.appendChild(option);
+        }
+        
+        const bookingStatusSelect = document.getElementById("booking_status");
+        const bookingStatuses = await getBookingStatuses(JSON.parse(this.value));
+        console.log(bookingStatuses)
+        for (const status of bookingStatuses.data) {
+            const option = document.createElement("option");
+            option.value = status.uid;
+            option.textContent = status.name;
+            bookingStatusSelect.appendChild(option)
+        }
+        
+    })    
+}
 
-    const bookingStatusSelect = document.getElementById("booking_status");
-    const bookingStatuses = await getBookingStatuses(JSON.parse(this.value));
-    console.log(bookingStatuses)
-    for (const status of bookingStatuses.data) {
-        const option = document.createElement("option");
-        option.value = status.uid;
-        option.textContent = status.name;
-        bookingStatusSelect.appendChild(option)
+export default async function buildPatientSelect(entityUid, patientUid = -1) {
+    const patientSelect = document.getElementById("patient_select")
+    if (patientSelect) {
+        const patients = await getPatients(entityUid);
+        patients.data.forEach(patient => {
+            const option = document.createElement("option");
+            option.value = patient.uid;
+            option.textContent = `${patient.name} ${patient.surname}`;
+            if (patientUid === patient.uid) {
+                option.selected = true;
+            }
+            patientSelect.appendChild(option);
+        })
     }
-})
+}
 
 async function getPatients(entityUid) {
     console.log(entityUid)
@@ -38,7 +52,7 @@ async function getPatients(entityUid) {
         credentials: "include"
     })
 
-    data = await res.json();
+    const data = await res.json();
     console.log(data);
     return data;
 
@@ -75,20 +89,25 @@ async function getBookingStatuses(diary) {
     return data
 }
 
-document.getElementById('create_booking').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    console.log(formData);
-    
-    const data = Object.fromEntries(formData.entries());
-    console.log(data)
+const createBookingForm = document.getElementById('create_booking')
+if (createBookingForm) {
+    createBookingForm.addEventListener('submit', async (e) => {
+        const formData = new FormData(e.target);
+        console.log(formData);
+        
+        const data = Object.fromEntries(formData.entries());
+        console.log(data)
 
-    const res = await fetch("http://localhost:3000/create", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include',
-        body: JSON.stringify(data)
-    })
-})
+        const res = await fetch("http://localhost:3000/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: 'include',
+            body: JSON.stringify(data)
+        })
+
+        window.location.href = "index.html";
+    })    
+}
+    
