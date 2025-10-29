ea1c811b7a70cc71bdd544b7614dc55f05b09d3d
ArmandleRoux98 <armand.leroux06@gmail.com>
2025-10-29 19:21:54 +0200

    Added documentation.

--- Calendar/server.js
+++ Calendar/server.js
@@ -12,11 +12,6 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    console.log(req.cookie)
-    res.send("Hello, world!");
-});
-
 // Login route
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
@@ -61,17 +56,12 @@ app.post('/login', async (req, res) => {
     }
 });
 
-// Get available diaries
+// Get available diaries and return them in JSON response.
 app.get("/diaries", async (req, res) => {
     const fields = [
         "uid",
         "entity_uid",
-        "treating_doctor_uid",
-        "service_center_uid",
-        "booking_type_uid",
-        "name",
-        "uuid",
-        "disabled",
+        "name"
     ]
     const filter = [
         "AND",
@@ -84,8 +74,6 @@ app.get("/diaries", async (req, res) => {
     const params = new URLSearchParams();
     params.append('fields', JSON.stringify(fields));
     const endPoint = `https://dev_interview.qagoodx.co.za/api/diary?${params.toString()}`;
-    console.log(endPoint);
-    console.log(req.cookies.session_id);
     try {
         const response = await fetch(endPoint, {
             method: "GET",
@@ -104,30 +92,21 @@ app.get("/diaries", async (req, res) => {
     };
 });
 
-
+// Retrieve bookings in diary with the given diary uid and on the specified date
 app.get("/bookings", async (req, res) => {
-    const { date, diary_uid, entity_uid } = req.query;
-    console.log(req.query);
-    console.log(date, diary_uid, entity_uid);
-    console.log(Number(diary_uid))
+    const { date, diary_uid } = req.query;
     const fields = [
         ["AS",["I","patient_uid","name"],"patient_name"],
         ["AS",["I","patient_uid","surname"],"patient_surname"],
-        ["AS",["I","patient_uid","debtor_uid","name"],"debtor_name"],
-        ["AS",["I","patient_uid","debtor_uid","surname"],"debtor_surname"],
         "uid",
         "entity_uid",
-        "diary_uid",
         "booking_type_uid",
         "booking_status_uid",
         "patient_uid",
         "start_time",
         "duration",
-        "treating_doctor_uid",
         "reason",
-        "invoice_nr",
-        "cancelled",
-        "uuid"
+        "cancelled"
     ]
     const filter = [
         "AND",
@@ -150,7 +129,6 @@ app.get("/bookings", async (req, res) => {
     params.append('fields', JSON.stringify(fields));
     params.append('filter', JSON.stringify(filter));
     const endPoint = `https://dev_interview.qagoodx.co.za/api/booking?${params.toString()}`;
-    console.log(endPoint);
     try {
         const response = await fetch(endPoint, {
             method: "GET",
@@ -160,8 +138,7 @@ app.get("/bookings", async (req, res) => {
         })
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}\nError: ${response.statusText}`);
         const data = await response.json();
-        
-        console.log("Success", data);
+
 
         res.json(data)
 
@@ -170,6 +147,7 @@ app.get("/bookings", async (req, res) => {
     };
 })
 
+// Retrieve booking with given booking uid
 app.get("/bookings/:uid", async (req, res) => {
     console.log(req.params.uid);
     const bookingUid = req.params.uid
@@ -192,7 +170,6 @@ app.get("/bookings/:uid", async (req, res) => {
     params.append('filter', JSON.stringify(filter));
 
     const endPoint = `https://dev_interview.qagoodx.co.za/api/booking?${params.toString()}`;
-    console.log(endPoint);
     try {
         const response = await fetch(endPoint, {
             method: "GET",
@@ -203,7 +180,6 @@ app.get("/bookings/:uid", async (req, res) => {
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}\nError: ${response.statusText}`);
         const data = await response.json();
         
-        console.log("Success", data);
         res.json(data);
 
 
@@ -212,14 +188,13 @@ app.get("/bookings/:uid", async (req, res) => {
     };
 })
 
+// Update booking with given booking uid
 app.post("/update/:uid", async (req, res) => {
     const uid = Number(req.params.uid)
     const date = `${req.body.booking_date}T${req.body.booking_time}:00`
     const duration = Number(req.body.duration)
     const patientUid = Number(req.body.patient)
     const reason = req.body.booking_reason
-    console.log(req.params.uid)
-    console.log(req.body.patient)
 
     const payload = {
         "model": 
@@ -232,10 +207,8 @@ app.post("/update/:uid", async (req, res) => {
             "cancelled": false
         }
     }   
-    console.log(payload)
 
     try {
-        console.log(req.cookies.session_id)
         const response = await fetch(`https://dev_interview.qagoodx.co.za/api/booking/${uid}`, {
             method: "PUT",
             headers: {
@@ -249,12 +222,15 @@ app.post("/update/:uid", async (req, res) => {
 
     const data = await response.json();
 
+    res.json(data);
+
     console.log("Booking Updated Successfully: ", data)
     } catch(error) {
         console.error("Error:", error);
     };
 })
 
+// Retrieves all booking types available for given entity and dairy uids
 app.get("/booking_type", async (req, res) => {
     const entityUid = req.query.entity_uid;
     const diaryUid = req.query.diary_uid;
@@ -262,12 +238,7 @@ app.get("/booking_type", async (req, res) => {
 
     const fields = [
         "uid",
-        "entity_uid",
-        "diary_uid",
-        "name",
-        "booking_status_uid",
-        "disabled",
-        "uuid"
+        "name"
     ]
     const filter = ["AND",
         ["=",
@@ -286,7 +257,6 @@ app.get("/booking_type", async (req, res) => {
     params.append('fields', JSON.stringify(fields));
     params.append('filter', JSON.stringify(filter));
     const endPoint = `https://dev_interview.qagoodx.co.za/api/booking_type?${params.toString()}`;
-    console.log(endPoint);
     try {
         const response = await fetch(endPoint, {
             method: "GET",
@@ -306,7 +276,7 @@ app.get("/booking_type", async (req, res) => {
     };
 })
 
-
+// Retrieves all booking statuses available for given entity and dairy uids
 app.get("/booking_status", async (req, res) => {
     const entityUid = Number(req.query.entity_uid);
     const diaryUid = Number(req.query.diary_uid);
@@ -314,13 +284,7 @@ app.get("/booking_status", async (req, res) => {
 
     const fields = [
         "uid",
-        "entity_uid",
-        "diary_uid",
-        "name",
-        "next_booking_status_uid",
-        "is_arrived",
-        "is_final",
-        "disabled"
+        "name"
     ]
     const filter = ["AND",
         ["=",
@@ -339,7 +303,6 @@ app.get("/booking_status", async (req, res) => {
     params.append('fields', JSON.stringify(fields));
     params.append('filter', JSON.stringify(filter));
     const endPoint = `https://dev_interview.qagoodx.co.za/api/booking_status?${params.toString()}`;
-    console.log(endPoint);
     try {
         const response = await fetch(endPoint, {
             method: "GET",
@@ -358,33 +321,14 @@ app.get("/booking_status", async (req, res) => {
     };
 })
 
-
+// Retrieves all patients available for given entity
 app.get("/patient", async (req, res) => {
     const entityUid = Number(req.query.entity_uid);
-    console.log(entityUid);
 
     const fields = [
         "uid",
-        "entity_uid",
-        "debtor_uid",
         "name",
         "surname",
-        "initials",
-        "title",
-        "id_type",
-        "id_no",
-        "date_of_birth",
-        "mobile_no",
-        "email",
-        "file_no",
-        "gender",
-        "dependant_no",
-        "dependant_type",
-        "acc_identifier",
-        "gap_medical_aid_option_uid",
-        "gap_medical_aid_no",
-        "private",
-        "patient_classification_uid"
     ]
     const filter = [
         "=",
@@ -397,7 +341,6 @@ app.get("/patient", async (req, res) => {
     params.append('filter', JSON.stringify(filter));
     params.append('limit', limit)
     const endPoint = `https://dev_interview.qagoodx.co.za/api/patient?${params.toString()}`;
-    console.log(endPoint);
     try {
         const response = await fetch(endPoint, {
             method: "GET",
@@ -407,8 +350,7 @@ app.get("/patient", async (req, res) => {
         })
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}\nError: ${response.statusText}`);
         const data = await response.json();
-        
-        console.log("Success", data);
+
         res.json(data);
 
     } catch(error) {
@@ -416,9 +358,11 @@ app.get("/patient", async (req, res) => {
     };
 })
 
+
+// Make API request to GXWeb to create new booking with given information
 app.post("/create", async (req, res) => {
-    console.log(req.body)
-    const diary = JSON.parse(req.body.diary)
+    console.log("Request has been made")
+    const diary = req.query
     const payload = {
         "model": {
             "entity_uid": Number(diary["entity_uid"]),
@@ -426,46 +370,48 @@ app.post("/create", async (req, res) => {
             "booking_type_uid": Number(req.body.booking_type),
             "booking_status_uid": Number(req.body.booking_status),
             "start_time": `${req.body.booking_date}T${req.body.booking_time}:00`,
-            "duration": 15,
+            "duration": Number(req.body.duration),
             "patient_uid": Number(req.body.patient),
             "reason": req.body.booking_reason,
             "cancelled": false
         }
     }
     console.log(payload)
-    try {
-        const response = await fetch("https://dev_interview.qagoodx.co.za/api/booking", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Cookie": `session_id=${req.cookies.session_id}`
-        },
-        body: JSON.stringify(payload)
-    });
+    if (payload.model.start_time.length === 19) {
+        console.log("Test")
+        try {
+            const response = await fetch("https://dev_interview.qagoodx.co.za/api/booking", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Cookie": `session_id=${req.cookies.session_id}`
+                },
+                body: JSON.stringify(payload)
+            });
+    
+            if (!response.ok) throw new Error(`HTTP error! Status: ${response.status} ${response.statusText}`);
+    
+            const data = await response.json();
 
-    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status} ${response.statusText}`);
-
-    const data = await response.json();
-
-    console.log("Booking Created Successfully: ", data)
-    } catch(error) {
-        console.error("Error:", error);
-    };
+            res.json(data);
+    
+            console.log("Booking Created Successfully: ", data)
+        } catch(error) {
+            console.error("Error:", error);
+        };
+    }
 })
 
-
+// Make API request to GXWeb to cancel booking with given booking uid
 app.put("/delete/:uid", async (req, res) => {
     const bookingUid = req.params.uid
-    console.log(bookingUid)
     const payload = {
         "model": {
             "uid": Number(bookingUid),
             "cancelled": true
         }
     }
-    console.log(payload)
     try {
-        console.log(req.cookies.session_id)
         const response = await fetch(`https://dev_interview.qagoodx.co.za/api/booking/${bookingUid}`, {
             method: "PUT",
             headers: {
@@ -479,6 +425,8 @@ app.put("/delete/:uid", async (req, res) => {
 
     const data = await response.json();
 
+    res.json(data);
+
     console.log("Booking Deleted Successfully: ", data)
     } catch(error) {
         console.error("Error:", error);
@@ -486,5 +434,5 @@ app.put("/delete/:uid", async (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Application running - listening on port ${port}`);
 })
